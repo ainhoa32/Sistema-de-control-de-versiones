2f86d372575974e4615497589eb5020092dab2a3
leila52 <lailahaddad385@gmail.com>
2023-12-12 12:43:01 +0100

    Merge branch 'master' of https://github.com/ainhoa32/Sistema-de-control-de-versiones.git

--- sistema_de_control_de_versiones/src/sistema_de_control_de_versiones/sistemaControl.java
+++ sistema_de_control_de_versiones/src/sistema_de_control_de_versiones/sistemaControl.java
@@ -6,15 +6,26 @@ public class sistemaControl {
 
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner (System.in);
-		
 		System.out.println("Introduce un lado: ");
 		int altura = entrada.nextInt();
 		System.out.println("Introduce otro lado: ");
 		int base = entrada.nextInt();
-		int base = entrada.nextInt();
-		int base = entrada.nextInt();
+		System.out.println("el area del trianfulo es"+calcularArea(base,altura););
+		System.out.println("Introduce el radio: ");
+		int radio = entrada.nextInt();
+		System.out.println("el area de la circunferencia es: "+calaACirc(radio););
+		System.out.println("el perimetro de la circunferencia es : "+perimetroACirc(radio););
 	}public static double calcularArea(int base, int altura) {
 		return base * altura / 2;
-	}
-
+	}public static double calaACirc(int radio) {
+		double area;
+		double pi=3.14;
+		area=pi*(radio*radio);
+		return area;
+	}public static double perimetroACirc(int radio) {
+		double pi=3.14;
+		double perimetro;
+		perimetro=2*(pi*radio);
+		return perimetro;
+}
 }
