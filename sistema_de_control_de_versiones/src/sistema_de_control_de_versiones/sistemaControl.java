b06d9ef776389df75e829bc7b8eb9c731d1fceee
leila52 <152276585+leila52@users.noreply.github.com>
2023-12-12 12:50:26 +0100

    Update sistemaControl.java
    12:50

--- sistema_de_control_de_versiones/src/sistema_de_control_de_versiones/sistemaControl.java
+++ sistema_de_control_de_versiones/src/sistema_de_control_de_versiones/sistemaControl.java
@@ -7,12 +7,11 @@ public class sistemaControl {
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner (System.in);
 		
-		System.out.println("Introduce un lado: ");
+		System.out.println("Introduce un altura: ");
 		int altura = entrada.nextInt();
-		System.out.println("Introduce otro lado: ");
+		System.out.println("Introduce otro base: ");
 		int base = entrada.nextInt();
-		int base = entrada.nextInt();
-		int base = entrada.nextInt();
+		System.out.println("el area del triangulo es : "+calcularArea(base,altura);
 	}public static double calcularArea(int base, int altura) {
 		return base * altura / 2;
 	}
